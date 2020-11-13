d1722fa9faf3599e75e2e6146997fa3016e24363
Jess Silva Carvalho <jc.carvalho@outlook.com>
2020-11-13 15:17:48 +0000

    heroku push

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  #Need to add ingredident_id reference too?
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    # @ingredient = Ingredient.find(params[:ingredient_id])
+    # @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
